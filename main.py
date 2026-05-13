c2c8f502920db42da62457b402e5ea309d67b163
Michael Bergman <mickos939@gmail.com>
2026-05-13 15:41:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,31 @@
-# To run and test the code you need to update 4 places:
 # 1. Change MY_EMAIL/MY_PASSWORD to your own details.
 # 2. Go to your email provider and make it allow less secure apps.
 # 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
 
 
 import os
 import smtplib
 import datetime as dt
-import pandas
 import random
 
 # import os and use it to get the Github repository secrets
 MY_EMAIL = os.environ.get("MY_EMAIL")
 MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
-today = dt.datetime.now()
-today_tuple = (dt.datetime.now().month, dt.datetime.now().day)
-
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+now = dt.datetime.now()
+weekday = now.weekday()
+#if weekday == 1:
+with open("quotes.txt") as quote_file:
+    all_quotes = quote_file.readlines()
+    quote = random.choice(all_quotes)
+print(quote)
 
 connection = smtplib.SMTP("smtp.gmail.com")
 connection.starttls()
 connection.login(user=MY_EMAIL, password=MY_PASSWORD)
 connection.sendmail(
     from_addr=MY_EMAIL,
-    to_addrs=birthday_person["email"],
-    msg=f"Subject:Grattis på födelsedagen!\n\n{contents}".encode("utf-8")
+    to_addrs="michael.bergman@katrineholm.se",
+    msg=f"Subject:Dagens citat\n\n{quote}".encode("utf-8")
 )
 connection.close()
