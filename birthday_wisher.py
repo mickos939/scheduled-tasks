799f530c669f5f8e0e2900ed882f650b28d44b7b
Michael Bergman <mickos939@gmail.com>
2026-05-14 16:33:35 +0200

    Update birthday_wisher.py

--- birthday_wisher.py
+++ birthday_wisher.py
@@ -1,10 +1,5 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
+# 1. Update the SMTP ADDRESS to match your email provider.
 # 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
 
 import os
 import smtplib
@@ -21,12 +16,15 @@ today_tuple = (dt.datetime.now().month, dt.datetime.now().day)
 
 data = pandas.read_csv("birthdays.csv")
 birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+
+if today_tuple not in birthdays_dict:
+    exit()
+
+birthday_person = birthdays_dict[today_tuple]
+file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
+with open(file_path) as letter_file:
+    contents = letter_file.read()
+    contents = contents.replace("[NAME]", birthday_person["name"])
 
 connection = smtplib.SMTP("smtp.gmail.com")
 connection.starttls()
